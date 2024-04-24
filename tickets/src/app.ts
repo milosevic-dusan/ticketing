24b2919faaca2e7e86bdc1425a11911f69e321e3
Dusan Milosevic <45673100+milosevic-dusan@users.noreply.github.com>
2024-04-24 12:55:40 +0200

    288: ticket service with create edit and get handlers and its tests

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
-import 'express-async-errors';
 import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError } from '@sh4nd00/common';
+import { errorHandler, NotFoundError, currentUser } from '@sh4nd00/common';
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
+import { updateTicketRouter } from './routes/update';
 
 const app = express();
 app.set('trust proxy', true);
@@ -14,6 +17,12 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
+app.use(currentUser);
+
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
@@ -22,5 +31,3 @@ app.all('*', async (req, res) => {
 app.use(errorHandler);
 
 export { app };
-
-
